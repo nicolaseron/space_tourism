882fef6b3b57c02773bdf3fd63fafb1dceec9f93
nicolaseron <nicoco@live.be>
2024-03-20 15:55:07 +0100

    test nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  image: {
+    provider: 'netlify',
+  },
   app: {
     pageTransition: { name: "page", mode: "out-in" },
   },
